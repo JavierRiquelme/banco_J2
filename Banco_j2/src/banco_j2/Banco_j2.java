ebb0b8914f9ab990e0149fc3254a5536b6709664
JavierRiquelme <jaricer258@gmail.com>
2019-02-07 10:16:03 +0100

    Modificación Banco_j2

--- Banco_j2/src/banco_j2/Banco_j2.java
+++ Banco_j2/src/banco_j2/Banco_j2.java
@@ -5,6 +5,7 @@ public class Banco_j2 {
 
     public static void main(String[] args) {
         System.out.println("Banco ejercicio 2 linea de comandos");
+        System.out.println("Modificación Banco ejercicio 2 linea de comandos");
     }
     
 }
